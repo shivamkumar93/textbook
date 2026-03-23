67fcb35ddcd8247be1a72f7082e01b882f0f3bb2
shivamkumar93 <bcashivam11@gmail.com>
2026-03-23 23:51:38 +0530

    create some page and books calling in homepage

--- nootbook/book/views.py
+++ nootbook/book/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'user_panel/home.html')
+    data = {
+        "title":"home",
+        "generes":Genere.objects.all(),
+        "books":Book.objects.all()
+    }
+    return render(request, 'user_panel/home.html', data)
--- nootbook/nootbook/urls.py
+++ nootbook/nootbook/urls.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 from book.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", home, name="homepage"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
