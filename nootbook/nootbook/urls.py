d302d8ebaa155e80a680046825b5b107fa522c84
shivamkumar93 <bcashivam11@gmail.com>
2026-03-24 16:12:07 +0530

    create admin dashboard page for manage all crud operations

--- nootbook/nootbook/urls.py
+++ nootbook/nootbook/urls.py
@@ -4,8 +4,13 @@ from django.urls import path
 from book.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from book.admin_views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", home, name="homepage"),
+
+
+# Admin urls Here
+    path("dashboard/", dashboard, name="admin_dashboard"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
