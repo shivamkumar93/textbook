61f3fa656999f47b40c315e0f7280d6c299f1172
shivamkumar93 <bcashivam11@gmail.com>
2026-03-25 00:05:58 +0530

    paginations works in admin panel

--- nootbook/book/admin_views.py
+++ nootbook/book/admin_views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, redirect
 from book.models import *
 from .forms import *
+from django.core.paginator import Paginator
+
+
+
+
 def dashboard(request):
     return render(request, "admin/dashboard.html")
 
 def manageGeneres(request):
     data = {}
     form = GenereForm(request.POST or None)
-    data['generes'] = Genere.objects.all()
+    generes = Genere.objects.all()
+    paginator = Paginator(generes, 10)
+    page_number = request.GET.get("page")
+    genere_obj = paginator.get_page(page_number)
+    data['generes'] = genere_obj
     data['form'] = form
 
     if request.method == 'POST':
@@ -22,7 +31,12 @@ def manageAuthor(request):
     data = {}
     form = AuthorForm(request.POST or None)
     data['form'] = form
-    data['authors'] = Author.objects.all()
+    authors = Author.objects.all()
+    paginator = Paginator(authors, 8)
+    page_number = request.GET.get("page")
+    author_obj = paginator.get_page(page_number)
+
+    data['authors'] = author_obj
 
     if request.method == 'POST':
         if form.is_valid():
@@ -34,7 +48,12 @@ def manageAuthor(request):
 
 def manageBooks(request):
     data = {}
-    data['books'] = Book.objects.all()
+    books = Book.objects.all()
+    # paginator 
+    paginator = Paginator(books, 5)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    data['books'] = page_obj
     return render(request, 'admin/manage_book.html', data)
 
 def insertBook(request):
